e1a49aa1811e8d3b194f3c958956079d810b42c0
Kartik11-web <kartiknz0011@gmail.com>
2025-07-09 22:01:15 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,31 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: '/',
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['icon-192.png', 'icon-512.png'],
+      manifest: {
+        name: "Kartik's Portfolio",
+        short_name: "Portfolio",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#4A148C",
+        icons: [
+          {
+            src: 'icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
